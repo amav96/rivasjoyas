c827012dfc01a16b4bd79b93cf50a719e67c9bb9
amav96 <alvaroamav96@gmail.com>
2021-02-05 16:55:38 -0300

    version

--- resources/views/home/master.blade.php
+++ resources/views/home/master.blade.php
@@ -9,8 +9,10 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="routeName" content="{{ URL::to('/') }}">
     <link rel="stylesheet" href="{{ url('assets/css/main.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+        integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"
         integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
@@ -20,16 +22,61 @@
 
 <body>
 
-    <div class="wrapper">
-        {{-- Aca deberia ir el nav y sidebar --}}
+    <div class="navbar" id="navbar">
+        <div class="navbar-section">
 
-        @section('content')
+            <div class="sub-navbar logo">
+                <img src="{{ asset('assets/img/static/logo-1.png') }}" alt="">
+            </div>
 
+        </div>
 
-        @show
+        <div class="navbar-section">
+            <div class="sub-navbar bars">
+                <i class="fas fa-bars"></i>
+            </div>
 
-        {{-- aca deberia ir el footer --}}
+        </div>
+
+        <div class="navbar-section nav-lg">
+
+            <div class="box-nav-bar">
+                <i class="fas fa-user"></i>
+                <span>Que compramos</span>
+            </div>
+            <div class="box-nav-bar">
+                <i class="fas fa-user"></i>
+                <span>Rolex</span>
+            </div>
+            <div class="box-nav-bar">
+                <i class="fas fa-user"></i>
+                <span>Diamantes</span>
+            </div>
+            <div class="box-nav-bar">
+                <i class="fas fa-user"></i>
+                <span>Contacto</span>
+            </div>
+
+        </div>
+
+        {{-- <a href="" class="menu-bars" id="show-menu">
+            <i class="fas fa-bars"></i>
+        </a> --}}
     </div>
+
+    <nav id="nav-menu">
+        <ul class="nav-menu-items">
+
+        </ul>
+    </nav>
+
+    @section('content')
+
+
+    @show
+
+    {{-- aca deberia ir el footer --}}
+
     <script src="{{ asset('assets/js/quote/main.js') }}"> </script>
 
 
